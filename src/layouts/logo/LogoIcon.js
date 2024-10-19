cd944e121709e1821c7361683c1b21753b79ed5c
Douglas Lundy <douglaslundy@gmail.com>
2024-10-19 17:22:28 -0300

    altered brasao 2

--- src/layouts/logo/LogoIcon.js
+++ src/layouts/logo/LogoIcon.js
@@ -2,13 +2,13 @@ import React from "react";
 import { Link } from "@mui/material";
 import Image from "next/image";
 import LogoDark from "../../../assets/images/logos/logo.png";
-import LogoDark2 from "../../../assets/images/logos/brasao.png";
+import Brasao from "../../../assets/images/logos/brasao.png";
 
 const LogoIcon = () => {
   return (
     <Link href="/">
       <Image width={200} height={80} src={LogoDark} alt={LogoDark} />
-      <Image width={200} height={80} src={LogoDark} alt={LogoDark2} />
+      <Image width={200} height={80} src={Brasao} alt={Brasao} />
     </Link>
   );
 };
