325187a69ddb3275bd4a7db24e10dc22a0cfdb5e
Douglas Lundy <douglaslundy@gmail.com>
2025-03-12 15:57:56 -0300

    altered dashboard

--- src/components/dashboard/Provisoria.js
+++ src/components/dashboard/Provisoria.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 const Dashboard = () => {
   // Insira o URL público gerado pelo Metabase
-  const iframeUrl = 'https://mb.dlsistemas.com.br/public/dashboard/4ece421e-7679-40dc-b0bd-76195dfcb64c';
+  // const iframeUrl = 'https://mb.dlsistemas.com.br/public/dashboard/4ece421e-7679-40dc-b0bd-76195dfcb64c';
+  const iframeUrl = 'https://lookerstudio.google.com/embed/reporting/30e41f91-0f96-42da-9911-717027216ff8/page/a86tD';
 
   return (
     <div>
