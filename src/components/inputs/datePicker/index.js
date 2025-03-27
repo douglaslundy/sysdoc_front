db658fc61ca9d582765e865a4a3b3f77d341832a
Douglas Lundy <douglaslundy@gmail.com>
2025-03-27 14:58:14 -0300

    fixed DatePicker Value error

--- src/components/inputs/datePicker/index.js
+++ src/components/inputs/datePicker/index.js
@@ -6,23 +6,27 @@ import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import ptBrLocale from 'date-fns/locale/pt-BR';
 
 export default function BasicDatePicker(props) {
+  const { label, name, value, setValue, disabled = false, sx } = props;
 
-    const { label, name, value, setValue, disabled = false, sx } = props;
+  const parsedValue =
+    value instanceof Date
+      ? value
+      : value
+      ? new Date(`${value}T12:00:00`)
+      : null;
 
-    return (
-        <LocalizationProvider
-            dateAdapter={AdapterDateFns}
-            adapterLocale={ptBrLocale}
-        >
-            <DatePicker
-                label={label}
-                name={name}
-                disabled={disabled}
-                views={['year', 'month', 'day']}
-                value={value}
-                onChange={(value) => { setValue(value) }}
-                renderInput={(params) => <TextField {...params} sx={sx} />}
-            />
-        </LocalizationProvider>
-    );
+  return (
+    <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={ptBrLocale}>
+      <DatePicker
+        label={label}
+        name={name}
+        disabled={disabled}
+        views={['year', 'month', 'day']}
+        value={parsedValue}
+        onChange={(newValue) => setValue(newValue)}
+        renderInput={(params) => <TextField {...params} sx={sx} />}
+      />
+    </LocalizationProvider>
+  );
 }
+
