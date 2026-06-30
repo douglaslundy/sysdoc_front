dc2684b761d246228a1acf0b969af591fcaed3c4
Douglas Lundy <douglaslundy@gmail.com>
2026-06-30 19:14:17 -0300

    Normaliza encoding do modulo de alvaras

--- src/components/alvaras/index.js
+++ src/components/alvaras/index.js
@@ -1,4 +1,4 @@
-﻿import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import {
     Box, Button, Chip, Fab, FormControl, InputLabel, MenuItem, Select,
     Table, TableBody, TableCell, TableContainer, TableHead, TableRow,
